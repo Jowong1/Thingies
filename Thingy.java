93a9cb09424f7b90a6c6d3d77117ad4d2a217fc8
Jowong1 <jowong1@s.sfusd.edu>
2018-10-25 08:29:36 -0700

    Done Classwork

--- Thingy.java
+++ Thingy.java
@@ -5,5 +5,17 @@ class Thingy
   {
     myX = myY = 0;
   }
+  public void setX(int x){
+    myX = x;
+  }
+  public void setY(int y){
+    myY = y;
+  }
+  public int getX(){
+    return myX;
+  }
+  public int getY(){
+    return myY;
+  }
   /* your java code for "getters" and "setters" here */
 }
